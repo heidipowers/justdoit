5046a8fcdf468fe658bed8c105796f99a18cd014
heidipowers <heidipowers@gmail.com>
2016-09-11 19:42:56 -0400

    Animations

--- js/main.js
+++ js/main.js
@@ -1,37 +1,76 @@
 $(document).ready(function() {
 
-let intro1 = $('.intro1');
-let unicrons = $('.unicrons');
-let $intro = $('.intro');
-let $development = $('.development');
-
- //TweenLite.fromTo(intro1, 3, {y:-100, opacity: 0}, {y:200, opacity: 1, ease:Elastic.easeOut})
- //TweenLite.fromTo(unicrons, 3, {x: 500, opacity: 1},{x:-2000, ease:Power1.easeIn, opacity: 0, onComplete:changeHeadline})
-
- tl = new TimelineLite();
-
- tl
-    .fromTo(intro1, 1, {y:-100, opacity: 0}, {y:200, opacity: 1, ease:Power1.easeOut, onComplete:changeHeadline})
-    .fromTo(unicrons, 2, {opacity: 1},{opacity: 0, onComplete:changeAgain})
-    .to(unicrons, 3, {width: 0, onUpdate:moveOnUp, ease:Power1.easeOut});
+    let intro1 = $('.intro1');
+    let unicrons = $('.unicrons');
+    let $intro = $('.intro');
+    let $development = $('.development');
 
 
- function changeHeadline(){
-  intro1.text("I create Things.");
- }
+    //Controller init
+    var controller = new ScrollMagic.Controller();
 
- function changeAgain(){
-  intro1.text("Let's tell the Story")
- }
 
- function moveOnUp(){
-  TweenLite.to(intro1, 1, {y:0})
+    // First Scene
+   var firstScene = new TimelineLite();
 
- }
+   var scene1 = new ScrollMagic.Scene({
+            triggerElement: unicrons,
+            // the scene should last for a scroll distance of 100px
+            offset: 50 // start this scene after scrolling for 50px
+        })
+        .addIndicators({
+          name: 'first'
+        })
+        .setTween(
+          firstScene
+            .fromTo(intro1, 1, { y: -100, opacity: 0 }, { y: 200, opacity: 1, ease: Power1.easeOut, onComplete: changeHeadline })
+            .fromTo(unicrons, 2, { opacity: 1 }, { opacity: 0, onComplete: changeAgain })
+            .to(unicrons, 3, { width: 0, onUpdate: moveOnUp, ease: Power1.easeOut })
+            )
+            .addTo(controller); // assign the scenes to the controller
+
+
+    //Second Scene
+
+    var secondScene = new TimelineLite();
+
+
+
+    var scene2 = new ScrollMagic.Scene({
+      triggerElement: ".scene2",
+
+      offset: 50
+    })
+    .addIndicators({
+      name: 'second'
+    })
+    .setTween(
+      secondScene
+       .to("h3", 1, {autoAlpha:0})
+       .fromTo(".fadein", 1, {opacity: 0}, {opacity: 1})
+      )
+    .addTo(controller);
+
+
+
+    //Animation Functions
+
+    //Scene 1
+    function changeHeadline() {
+        intro1.text("I'm learning to create good things.");
+    }
+
+    function changeAgain() {
+        intro1.text("Let me tell you my story...")
+    }
+
+    function moveOnUp() {
+        TweenLite.to(intro1, 1, { y: 0 })
+
+    }
+
+
 
 
 
 });
-
-
-
